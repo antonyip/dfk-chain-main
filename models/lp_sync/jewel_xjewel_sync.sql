3714159142ffaed92757bd01c65d4fa33c95b750
Anton Yip <Anton_Yip@hotmail.com>
2022-05-23 05:17:25 -0700

    lp sync events

---
+++ models/lp_sync/jewel_xjewel_sync.sql
@@ -0,0 +1,54 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='day_date',
+        tags=['core', 'sync'],
+        cluster_by=['day_date']
+    )
+}}
+
+with raws as (
+    select 
+        date_trunc('day', block_timestamp) as day_date,
+        block_timestamp,
+        data
+    from logs
+    where {{ incremental_last_x_days('block_timestamp', 2) }}
+        and address = lower('0x6AC38A4C112F125eac0eBDbaDBed0BC8F4575d0d') -- jewel - xjewel
+        and topic0 = '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1' -- sync
+        
+),
+counter as (
+    select 
+        day_date,
+        data,
+        row_number() over (partition by day_date order by block_timestamp desc) as rnk
+    from raws
+),
+clean_sync as (
+    select
+        *
+    from counter
+    where rnk = 1
+), 
+final as (
+    select 
+        day_date,
+        hex_to_decimal(
+            trim(
+                LEADING 
+                '0'
+                from substr(data,3, 64)
+            )
+        )::numeric / power(10,18) as token0,
+        hex_to_decimal(
+            trim(
+                LEADING 
+                '0'
+                from substr(data,3+64, 64)
+            )
+        )::numeric / power(10,18) as token1
+    from clean_sync
+)
+
+select * from final
